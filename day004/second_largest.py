f88f9ea9a76f7c4bd915efb74c1982aadde19f4b
mayldute <mayldute@icloud.com>
2026-08-13 17:21:04 +0200

    implement second_largest

---
+++ day004/second_largest.py
@@ -0,0 +1,32 @@
+"""
+Task:
+Find the second largest unique integer in a list.
+
+Requirements:
+- Return the second largest unique integer.
+- Duplicate values should not affect the result.
+- Return None if there are fewer than two unique values.
+- Do not use sorted() or list.sort().
+- Do not use set().
+- Do not modify the original list.
+- Traverse the list only once.
+- Aim for O(n) time complexity.
+- Aim for O(1) additional space.
+"""
+
+def second_largest(items: list[int]) -> int | None:
+
+    if len(items) < 2:
+        return None
+
+    largest = items[0]
+    second = None
+
+    for i in items[1:]:
+        if items[i] < largest and (second is None or items[i] > second):
+            second = items[i]
+        elif items[i] > largest:
+            second = largest
+            largest = items[i]
+
+    return second

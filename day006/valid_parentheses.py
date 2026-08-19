73e5d6132f6c535b8ee01dfaf08cc40641d4ee25
mayldute <mayldute@icloud.com>
2026-08-19 15:56:05 +0200

    add is_valid_parentheses func

---
+++ day006/valid_parentheses.py
@@ -0,0 +1,28 @@
+"""
+Task:
+Determine whether a string of brackets is valid.
+
+Requirements:
+- The string may contain (), {}, and [].
+- Every opening bracket must have a matching closing bracket.
+- Brackets must close in the correct order.
+- Return True if the string is valid.
+- Return False otherwise.
+- Aim for O(n) time complexity.
+"""
+
+
+def is_valid_parentheses(text: str) -> bool:
+    stack = []
+    brackets = {')': '(', ']': '[', '}': '{'}
+
+    for char in text:
+        if char in brackets.values():
+            stack.append(char)
+        elif char in brackets:
+            if stack and stack[-1] == brackets[char]:
+                stack.pop()
+            else:
+                return  False
+
+    return not stack

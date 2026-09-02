c6f6dd8d179c1044acff4199ff3ca2083b76f543
mayldute <mayldute@icloud.com>
2026-09-02 14:54:16 +0200

    rotate_matrix_v2 func

--- day013/rotate_matrix.py
+++ day013/rotate_matrix.py
@@ -27,3 +27,31 @@ def rotate_matrix_v1(matrix: list[list[int]]) -> list[list[int]]:
         col_num += 1
 
     return new_matrix
+
+
+def rotate_matrix_v2(matrix: list[list[int]]) -> list[list[int]]:
+    new_matrix = [[0 for _ in range(len(matrix))] for _ in range(len(matrix))]
+    current_layer = 0   # current layer: 0 = outside, 1 = one step inside, 2 = two steps inside
+
+    if len(matrix) % 2 != 0:
+        center = len(matrix) // 2
+        new_matrix[center][center] = matrix[center][center]
+
+    while current_layer < len(matrix) // 2:
+        last = len(matrix) - 1 - current_layer  # opposite edge of the current layer
+
+        for col_idx in range(current_layer, last + 1):
+            new_matrix[col_idx][last] = matrix[current_layer][col_idx]
+
+        for row_idx in range(current_layer + 1, last):
+            new_matrix[last][row_idx] = matrix[- 1 - row_idx][last]
+
+        for col_idx in range(current_layer, last + 1):
+            new_matrix[col_idx][current_layer] = matrix[last][col_idx]
+
+        for row_idx in range(current_layer + 1, last):
+            new_matrix[current_layer][row_idx] = matrix[- 1 - row_idx][current_layer]
+
+        current_layer += 1
+
+    return new_matrix
